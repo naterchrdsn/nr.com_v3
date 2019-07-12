7a266f3465e3d637f9f00f27464961d67315dc58
Nate Richardson <hello@naterichardson.com>
2019-07-12 18:04:37 -0400

    finished new layout. super minimalist for now

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,50 +1,106 @@
-
 export default {
   mode: 'spa',
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
-    title: process.env.npm_package_name || '',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    title: 'Nate Richardson - Maker, Gamer, Breakfast Enthusiast',
+    meta: [{
+        charset: 'utf-8'
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Software Engineer with a passion for helping people do what they love, better.'
+      },
+      {
+        name: 'twitter:card',
+        content: 'summary'
+      }, {
+        name: 'twitter:site',
+        content: '@naterchrdsn'
+      }, {
+        name: 'twitter:title',
+        content: 'Website of Nate Richardson'
+      }, {
+        name: 'twitter:description',
+        content: 'Software Engineer with a passion for helping people do what they love, better.'
+      }, {
+        name: 'twitter:creator',
+        content: '@naterchrdsn'
+      }, {
+        name: 'twitter:image',
+        content: 'http://richardsonmediahouse.com/sy-misc/fulllogo.png'
+      }, {
+        property: 'og:title',
+        content: 'Website of Nate Richardson'
+      }, {
+        property: 'og:url',
+        content: 'https://naterichardson.com'
+      }, {
+        property: 'og:image',
+        content: 'http://richardsonmediahouse.com/sy-misc/fulllogo.png'
+      }, {
+        property: 'og:description',
+        content: 'Software Engineer with a passion for helping people do what they love, better.'
+      }, {
+        property: 'og:site_name',
+        content: 'NateRichardson.com'
+      },
+      {
+        name: 'msvalidate.01',
+        content: 'D4DC91DA48E7BB91BAF7F01E1C3CF050'
+      }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{
+      rel: 'icon',
+      type: 'image/x-icon',
+      href: '/favicon.ico'
+    }]
   },
   /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
+   ** Customize the progress-bar color
+   */
+  loading: {
+    color: '#fff'
+  },
   /*
-  ** Global CSS
-  */
-  css: [
-  ],
+   ** Global CSS
+   */
+  css: [],
   /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-  ],
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
   /*
-  ** Nuxt.js modules
-  */
+   ** Nuxt.js modules
+   */
   modules: [
     // Doc: https://buefy.github.io/#/documentation
     'nuxt-buefy',
     '@nuxtjs/pwa',
+    ['nuxt-fontawesome', {
+      component: 'fa',
+      imports: [{
+        set: '@fortawesome/free-brands-svg-icons',
+        icons: ['faTwitter', 'faGithub', 'faLinkedin']
+      }, {
+        set: '@fortawesome/free-solid-svg-icons',
+        icons: ['faEnvelope']
+      }]
+    }],
   ],
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** You can extend webpack config here
-    */
-    extend(config, ctx) {
-    }
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
   }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,79 +1,120 @@
 <template>
   <main>
-    <section class="hero is-fullheight">
-      <video loop muted autoplay class="bg-video sweet-background">
-        <source src="@/static/bgloop.mp4" type="video/mp4">
-      </video>
+    <section class="hero is-fullheight splash">
       <div class="hero-body">
         <div class="container has-text-centered">
-          <h1 class="title">
-            Nate Richardson
-          </h1>
-          <h2 class="subtitle">
-            Maker, Gamer, Breakfast Enthusiast
-          </h2>
+          <h1 class="title">Nate Richardson</h1>
+          <h2 class="subtitle">Maker, Gamer, Breakfast Enthusiast</h2>
         </div>
       </div>
     </section>
-    <section class="hero is-fullheight">
-      <h1 class="section-heading">Hi there, I'm Nate!</h1>
-          <h3 class="section-subheading">I am a Software Developer with a passion for creating and optimizing technology to help people do what they love, better.</h3>
-          <span class="divider"></span>
-          <div class="about-wrapper">
-              <img class="headshot" src="img/headshot.jpg" alt="headshot">
-              <div class="about-text-wrapper">
-                  <p class="about-text"><strong>I'm a Software Developer & Photographer @ Richardson Media House</strong> based in Atlanta, Georgia. I have extensive experience prototyping, designing, developing, and optimizing applications and UIs for the Web. I enjoy creating software that is both lightweight and functional, while going above and beyond to bring quality code that fulfills its intended use.</p>
-                  <p class="about-text">I bring almost a decade of development experience, over a decade of experience building and maintaining all types of computers, as well as a penchant for the details of minimalist design. Ultimately, I enjoy learning about and using different technology and languages to keep current in the industry and always know what tech is good for a specific use-case.</p>
-                  <p class="about-text"><strong>When I'm not building an application or computer,</strong> I'm probably hanging out with my son, Luca, or photographing something.</p>
-              </div>
+    <section class="section about">
+      <div class="container">
+        <h1 class="title">Hi there, I'm Nate!</h1>
+        <h2
+          class="subtitle"
+        >I am a Software Engineer with a passion for helping people do what they love, better.</h2>
+        <div class="about-content-wrapper level">
+          <div class="about-text-wrapper">
+            <p class="about-text">
+              <strong>I'm Co-Owner of Richardson Media House, and a Full-Stack Software Engineer at charterUP</strong>, both based in Atlanta, Georgia. I have extensive experience prototyping, designing, developing, and optimizing applications and UIs for the Web. I enjoy creating software that is both lightweight and functional, while going above and beyond to bring quality code that fulfills its intended use.
+            </p>
+            <br />
+            <p
+              class="about-text"
+            >I bring about a decade of software architecture and engineering experience, and over a decade of experience building and maintaining all types of computers. Ultimately, I enjoy learning about and using different technology and languages to keep current in the industry and always know what tech is good for a specific use-case.</p>
+            <br />
+            <p class="about-text">
+              <strong>When I'm not building an application or computer,</strong> I'm probably hanging out with my amazing family, or photographing something.
+            </p>
+            <br />
+            <div class="level">
+              <p class="level-left">
+                Here's a few things I'm experienced with:
+                <br />&nbsp;&nbsp;❯ JavaScript
+                <br />&nbsp;&nbsp;❯ TypeScript
+                <br />&nbsp;&nbsp;❯ HTML & CSS
+                <br />&nbsp;&nbsp;❯ Angular
+                <br />&nbsp;&nbsp;❯ Vue
+                <br />&nbsp;&nbsp;❯ Node.js
+              </p>
+              <p class="level-right">
+                <a
+                  style="margin-right: 150px;"
+                  class="button is-outlined is-link is-large"
+                  href="https://www.dropbox.com/s/swwugjps0kqxirh/Nate_Richardson_Software_Engineer2.pdf?dl=0"
+                >View My Resume</a>
+              </p>
+            </div>
           </div>
+          <div class="has-text-centered">
+            <figure class="image is--4by5">
+              <img class="headshot" src="@/static/smaller.jpg" alt="headshot" />
+            </figure>
+            <hr />
+            <h3>Let's Talk!</h3>
+            <span>
+              <a href="https://twitter.com/naterchrdsn">
+                <fa class="icon is-medium" :icon="['fab', 'twitter']" />
+              </a>
+              <a href="https://github.com/naterchrdsn">
+                <fa class="icon is-medium" :icon="['fab', 'github']" />
+              </a>
+              <a href="https://linkedin.com/in/naterchrdsn">
+                <fa class="icon is-medium" :icon="['fab', 'linkedin']" />
+              </a>
+              <a href="mailto:hello@naterichardson.com">
+                <fa class="icon is-medium" :icon="['fas', 'envelope']" />
+              </a>
+            </span>
+          </div>
+        </div>
+      </div>
     </section>
+    <footer class="footer">
+      <div class="content has-text-centered">
+        <p>
+          ©
+          <span id="copyright-year">{{ getCurrentYear() }}</span> Copyright: Devlytic LLC.
+        </p>
+      </div>
+    </footer>
   </main>
 </template>
 
 <script>
-import Card from '~/components/Card'
-
 export default {
   name: 'HomePage',
-
-  components: {
-    Card
+  methods: {
+    getCurrentYear() {
+      return new Date().getFullYear()
+    }
   }
 }
 </script>
 
-<style>
-  @font-face {
-    font-family: Nilland;
-    src: url('/assets/fonts/Nilland.ttf');
-  }
-  @font-face {
-    font-family: Nilland-Black;
-    src: url('/assets/fonts/Nilland-Black.ttf');
-  }
-  @font-face {
-    font-family: Nilland-Bold;
-    src: url('/assets/fonts/Nilland-Bold.ttf');
-  }
-  @font-face {
-    font-family: Nilland-SmallCaps;
-    src: url('/assets/fonts/Nilland-SmallCaps.ttf');
-  }
-  .sweet-background {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    width: auto;
-    height: auto;
-    z-index: -100;
-    min-width: 100%;
-    min-height: 100%;
-    -webkit-transform: translate(-50%, -50%);
-        -moz-transform: translate(-50%, -50%);
-        -ms-transform: translate(-50%, -50%);
-        transform: translate(-50%, -50%);
-  }
+<style lang="scss">
+@font-face {
+  font-family: Nilland;
+  src: url('/fonts/Nilland.ttf');
+}
+@font-face {
+  font-family: Nilland-Black;
+  src: url('/fonts/Nilland-Black.ttf');
+}
+@font-face {
+  font-family: Nilland-Bold;
+  src: url('/fonts/Nilland-Bold.ttf');
+}
+@font-face {
+  font-family: Nilland-SmallCaps;
+  src: url('/fonts/Nilland-SmallCaps.ttf');
+}
+.splash {
+  background-image: url('/splash.jpg');
+  background-size: cover;
+  background-position: top;
+
   .title {
     color: #fff;
     font-family: 'Nilland';
@@ -84,5 +125,28 @@ export default {
     font-family: 'Nilland-SmallCaps';
     text-transform: uppercase;
   }
+}
+.about {
+  .image {
+    border-radius: 200px;
+    overflow: hidden;
+    height: 250px;
+    width: 250px;
+
+    .headshot {
+      top: -75px;
+      position: relative;
+    }
+  }
+  .about-text-wrapper {
+    display: inline-block;
+    width: 75%;
+  }
+}
+.footer {
+  .logo {
+    width: 50px;
+  }
+}
 </style>
 
